4cf2f1821be04844fd1ef9b93f37872dd8411991
oldwinter <cdd2zju@gmail.com>
2023-08-22 21:57:36 +0800

    改变主页配置

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,13 +3,13 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🌱 oldwinterの数字花园",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "garden.oldwinter.top",
     ignorePatterns: ["private", "templates"],
     theme: {
       typography: {
